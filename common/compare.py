d9687a0a0bb30b659ae12d6fc31965f5af8c605d
wangmengmeng <wmm_0165@163.com>
2020-03-27 13:09:39 +0800

    修改compare.py

--- common/compare.py
+++ common/compare.py
@@ -6,10 +6,18 @@
 def listdict_compare(list1, list2):
     """两个列表格式同为 列表里嵌套字典（格式如[{'id': 1002, 'pharmacist_id': '3', 'pharmacist_name': 'wangmm'}]） 比较是否相等
     """
-    if len(list1) != list2:
+    if len(list1) != len(list2):
         return 0
     for i in list1:
         if i in list2:
             continue
-        return i
+        return 0
     return 1
+
+if __name__ == '__main__':
+
+    list1 = [1, 4]
+    list2 = [1, 3]
+    list3 = [1, 4]
+    print(listdict_compare(list1, list2))
+    print(listdict_compare(list1, list3))
---
+++ demo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/22 20:16
+# @Author : wangmengmeng
+def baihe(a, b, c):
+    if a > 0 and b > 0:
+        c = c / a
+    if a > 1 or c > 1:
+        c = c + 1
+    c = b + c
